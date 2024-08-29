1d69ac17e4475fdcbfdf2583fb7c3a0b266c6124
techysiddhant <sidjain8000@gmail.com>
2024-08-29 16:34:29 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello Sid</h1>
+      <Button>Hey</Button>
+    </div>
+  );
+}
