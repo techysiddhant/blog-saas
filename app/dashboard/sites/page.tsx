1bd627c1feed4ddf8e69599b3ed1b7e81ad80feb
techysiddhant <sidjain8000@gmail.com>
2024-08-31 00:01:45 +0530

    complete server action to create site

--- app/dashboard/sites/page.tsx
+++ app/dashboard/sites/page.tsx
@@ -1,8 +1,29 @@
+import prisma from "@/app/utils/db";
 import { Button } from "@/components/ui/button"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { PlusCircle } from "lucide-react"
 import Link from "next/link"
+import { redirect } from "next/navigation";
+async function getData(userId: string) {
+    const data = await prisma.site.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
 
-const SitesRoute = () => {
+    return data;
+}
+const SitesRoute = async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/api/auth/login");
+    }
+    const data = await getData(user.id);
     return (
         <>
             <div className="flex w-full justify-end">
---
+++ app/utils/zodSchemas.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+export const siteSchema = z.object({
+  name: z.string().min(1).max(35),
+  description: z.string().min(1).max(150),
+  subdirectory: z.string().min(1).max(40),
+});
