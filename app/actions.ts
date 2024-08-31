729fb4e71efc62455bf76e43367cdf1638ed0b7b
techysiddhant <sidjain8000@gmail.com>
2024-08-31 20:24:05 +0530

    add a create article page

--- app/actions.ts
+++ app/actions.ts
@@ -2,8 +2,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
-import { siteSchema } from "./utils/zodSchemas";
+import { PostSchema, siteSchema } from "./utils/zodSchemas";
 import prisma from "./utils/db";
+import { requireUser } from "./utils/requireUser";
 export async function CreateSiteAction(prevState: any, formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
@@ -27,4 +28,30 @@ export async function CreateSiteAction(prevState: any, formData: FormData) {
     },
   });
   return redirect("/dashboard/sites");
+}
+
+export async function CreatePostAction(prevState: any, formData: FormData) {
+  const user = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: PostSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.post.create({
+    data: {
+      title: submission.value.title,
+      smallDescription: submission.value.smallDescription,
+      slug: submission.value.slug,
+      articleContent: JSON.parse(submission.value.articleContent),
+      image: submission.value.coverImage,
+      userId: user.id,
+      siteId: formData.get("siteId") as string,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
 }
