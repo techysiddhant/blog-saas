aca9d970f2c97cc10fc76ebe3e5cd4f8f4a4b5ee
techysiddhant <sidjain8000@gmail.com>
2024-09-01 01:39:23 +0530

    complete the settings page

--- app/actions.ts
+++ app/actions.ts
@@ -94,3 +94,31 @@ export async function DeletePost(formData: FormData) {
 
   return redirect(`/dashboard/sites/${formData.get("siteId")}`);
 }
+export async function UpdateImage(formData: FormData) {
+  const user = await requireUser();
+
+  const data = await prisma.site.update({
+    where: {
+      userId: user.id,
+      id: formData.get("siteId") as string,
+    },
+    data: {
+      imageUrl: formData.get("imageUrl") as string,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
+
+export async function DeleteSite(formData: FormData) {
+  const user = await requireUser();
+
+  const data = await prisma.site.delete({
+    where: {
+      userId: user.id,
+      id: formData.get("siteId") as string,
+    },
+  });
+
+  return redirect("/dashboard/sites");
+}
