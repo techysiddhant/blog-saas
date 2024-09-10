3e9b8206ecb3212578e1dd901cc1abe0b6ce2fe1
techysiddhant <sidjain8000@gmail.com>
2024-09-10 18:26:52 +0530

    completed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,19 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Hero } from "./components/home/Hero";
+import { Features } from "./components/home/Features";
+import { PricingTable } from "./components/shared/Pricing";
+import { Logos } from "./components/home/Logos";
 export default async function Home() {
   const { getUser } = getKindeServerSession();
   const session = await getUser();
   return (
-    <div className="p-10">
-      <h1>Hello Sid</h1>
-      {
-        session ? <LogoutLink>
-          <Button>Logout</Button>
-        </LogoutLink> : (
-          <>
-            <RegisterLink>
-              <Button>
-                Register
-              </Button>
-            </RegisterLink>
-            <LoginLink>
-              <Button>Login</Button>
-            </LoginLink>
-          </>
-        )
-      }
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <Hero />
+      <Logos />
+      <Features />
+      <PricingTable />
     </div>
   );
 }
